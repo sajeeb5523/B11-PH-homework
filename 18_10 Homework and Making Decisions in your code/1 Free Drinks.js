25bac08ac55729aba89203829248823c798ba975
sajeeb5523 <sajeebaljabed1@gmail.com>
2026-02-19 17:06:03 +0600

    milestone 04, module 18-10 homework done

---
+++ 18_10 Homework and Making Decisions in your code/1 Free Drinks.js
@@ -0,0 +1,20 @@
+
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+
+
+
+let burgerPrice = 550;
+
+if (burgerPrice > 500) {
+    console.log("You get a free Coke!");
+}
+else{
+    console.log("Coke price: 30tk");
+}
